523a80377528091d6ce8d1ef5e5ea668801a405c
newor0599 <rowen778623@gmail.com>
2025-08-30 10:09:43 +0800

    - Added light/dark theme selection - Tweaked area script - Tweaked mango config

--- .config/mango/autostart.sh
+++ .config/mango/autostart.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set +e
-#wlr-randr --output HDMI-A-1 --scale 0.711 --pos 0,0 &
-#wlr-randr --output eDP-1 --scale 1 --pos 0,1080 &
 ignis init &
 dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots &
-# /usr/lib/xdg-desktop-portal &
 /usr/lib/xdg-desktop-portal &
 # /usr/lib/xdg-desktop-portal-wlr &
 # /usr/lib/xdg-desktop-portal-gtk &
